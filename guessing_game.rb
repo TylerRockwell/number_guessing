e2f836bd0439762a1831caa0e6ecda4331020308
Tyler Rockwell <tyler.rockwell87@gmail.com>
2015-09-01 13:42:55 -0400

    Add new game option

--- guessing_game.rb
+++ guessing_game.rb
@@ -35,50 +35,65 @@ def warn_player(warnings)
   when 2
     puts "LAST WARNING!"
   else
-    terminate("I asked nicely. Now you're done")
+    terminate("I asked nicely. Now you're done.")
   end
 end
 
+def keep_playing
+  puts ""
+  puts "Would you like to play again? (y/n)"
+  gets.chomp == "y" ? (true) : (false)
+end
 
 
-answer = rand(1..100)
-correct = false
-warnings = 0
-num_guess = 0
-suffix = ""
-puts ""
-puts "##################################################"
-puts "#        Welcome my number guessing game.        #"
-puts "#  I will think of a number between 1 and 100.   #"
-puts "#  You will guess what that number is (or not).  #"
-puts "#                                                #"
-puts "#      Type exit at anytime to terminate         #"
-puts "##################################################"
-puts ""
-#Initial prompt
+continue = true
 
-while (num_guess < 5)
-  #Input guess from user
-  num_guess += 1
-  guess = get_input
-  terminate("Leaving so soon? Goodbye then.") if guess == "exit"
+#New game loop
+while continue
+  #Title Prompt
+  puts ""
+  puts "##################################################"
+  puts "#        Welcome my number guessing game.        #"
+  puts "#  I will think of a number between 1 and 100.   #"
+  puts "#  You will guess what that number is (or not).  #"
+  puts "#                                                #"
+  puts "#      Type exit at anytime to terminate         #"
+  puts "##################################################"
+  puts ""
 
-  #Ensure that guess is a number
-  if is_a_number(guess)
-    guess = guess.to_i
-    correct = check_guess(guess, answer)
-    break if correct
+  #Initialize new game
+  answer = rand(1..100)
+  correct = false
+  warnings = 0
+  num_guess = 0
+  suffix = ""
+  #Main game loop
+  while (num_guess < 5)
+    #Input guess from user
+    guess = get_input
+    terminate("Leaving so soon? Goodbye then.") if guess == "exit"
+
+    #Ensure that guess is a number
+    if is_a_number(guess)
+      num_guess += 1
+      guess = guess.to_i
+      correct = check_guess(guess, answer)
+      break if correct
+    else
+      #Warn, and eventually terminate for non-numbers
+      warn_player(warnings)
+      warnings+=1
+    end
+  end
+
+  #Check if player found the right answer and end the game
+  if correct
+    suffix = "es" if num_guess > 1
+    puts "Congratulations! You guessed correctly in #{num_guess} guess#{suffix}."
   else
-    warn_player(warnings)
-    warnings+=1
+    puts "Sorry, you did not guess the right number. The correct number was #{answer}."
   end
+  #Ask if player wants to play again
+  continue = keep_playing
 end
-
-if correct
-  suffix = "es" if num_guess > 1
-  puts "Congratulations! You guessed correctly in #{num_guess} guess#{suffix}."
-else
-  puts "Sorry, you did not guess the right number. The correct number was #{answer}."
-end
-
 puts ""
